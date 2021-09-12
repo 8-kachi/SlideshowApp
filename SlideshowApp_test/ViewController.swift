e829ae7784aa218a5081d8a3d69f98fec253c6cf
8_kachi_ <ponta510000@gmail.com>
2021-09-12 15:58:55 +0900

    Initial Commit

---
+++ SlideshowApp_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp_test
+//
+//  Created by 浅野総一郎 on 2021/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
